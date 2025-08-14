6e258bfe119c3a30249578a1b87c6c3237494c7e
Real-Ibrahim <m.ibrahim172501@gmail.com>
2025-08-14 14:55:50 +0500

    Added Persistent Counter

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() {
   runApp(MyApp());
@@ -11,6 +12,13 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Counter App',
+      theme: ThemeData(
+        primaryColor: Color(0xFF4A90E2),
+        scaffoldBackgroundColor: Color(0xFFF4F6F8),
+        textTheme: TextTheme(
+          bodyMedium: TextStyle(fontSize: 18, fontWeight: FontWeight.w500),
+        ),
+      ),
       home: CounterScreen(),
     );
   }
@@ -27,34 +35,91 @@ class CounterScreen extends StatefulWidget {
 class _CounterScreenState extends State<CounterScreen> {
   int _counter = 0;
 
+  @override
+  void initState() {
+    super.initState();
+    _loadCounter();
+  }
+
+  Future<void> _loadCounter() async {
+    final prefs = await SharedPreferences.getInstance();
+    setState(() {
+      _counter = prefs.getInt('counter') ?? 0;
+    });
+  }
+
+  Future<void> _saveCounter() async {
+    final prefs = await SharedPreferences.getInstance();
+    prefs.setInt('counter', _counter);
+  }
+
   void _increment() {
-    setState(() { _counter++; });
+    setState(() => _counter++);
+    _saveCounter();
   }
 
   void _decrement() {
-    setState(() { _counter--; });
+    setState(() => _counter--);
+    _saveCounter();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Counter')),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text('Counter: $_counter'),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                ElevatedButton(onPressed: _decrement, child: Text('-')),
-                SizedBox(width: 16),
-                ElevatedButton(onPressed: _increment, child: Text('+')),
-              ],
-            ),
-          ],
+      appBar: AppBar(
+        title: Text('Counter', style: TextStyle(fontWeight: FontWeight.w600)),
+        backgroundColor: Color(0xFF4A90E2),
+        elevation: 2,
+      ),
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'Your Count',
+                style: TextStyle(
+                  fontSize: 22,
+                  color: Colors.grey[700],
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+              SizedBox(height: 8),
+              Text(
+                '$_counter',
+                style: TextStyle(
+                  fontSize: 60,
+                  fontWeight: FontWeight.bold,
+                  color: Color(0xFF4A90E2),
+                ),
+              ),
+              SizedBox(height: 30),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  _buildActionButton(Icons.remove, _decrement, Colors.redAccent),
+                  SizedBox(width: 20),
+                  _buildActionButton(Icons.add, _increment, Colors.green),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
+    );
+  }
+
+  Widget _buildActionButton(IconData icon, VoidCallback onPressed, Color color) {
+    return ElevatedButton(
+      onPressed: onPressed,
+      style: ElevatedButton.styleFrom(
+        shape: CircleBorder(),
+        padding: EdgeInsets.all(20),
+        backgroundColor: color,
+        elevation: 5,
+        shadowColor: Colors.black26,
+      ),
+      child: Icon(icon, size: 28, color: Colors.white),
     );
   }
 }
